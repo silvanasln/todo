34c1b2dcc50f8a1840989064283e8d759bd784ae
silvanasln <silvanasalsabilian@gmail.com>
2025-05-31 20:01:21 +0330

    Update todolist.go

--- todolist.go
+++ todolist.go
@@ -1,36 +1,57 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
+ "context"
+ "encoding/json"
+ "fmt"
+ "log"
+ "net/http"
+
+ "go.mongodb.org/mongo-driver/mongo"
+ "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	var todolist []string
+ client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
 
-	todolist = append(todolist, "خرید کردن")
-	todolist = append(todolist, "کتاب خوندن")
-	todolist = append(todolist, "فیلم دیدن")
-	todolist = append(todolist, "پختن غذا")
+ }
+ defer client.Disconnect(context.TODO()) 
 
-	fmt.Println("کارهایی که امروز باید انجام بدم:")
-	for i, harkar := range todolist {
-		fmt.Println(i+1, ".", harkar)
-	}
+ db := client.Database("todoDB")      
+ collection := db.Collection("tasks")   
 
-	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			w.Header().Set("Content-Type", "application/json")
-			if err := json.NewEncoder(w).Encode(todolist); err != nil {
-				http.Error(w, "خطا در تبدیل به JSON", http.StatusInternalServerError)
-				return
-			}
-		} else {
-			http.Error(w, "فقط متد GET پشتیبانی می‌شه", http.StatusMethodNotAllowed)
-		}
-	})
+ var todolist []string
+ todolist = append(todolist, "خرید کردن")
+ todolist = append(todolist, "کتاب خوندن")
+ todolist = append(todolist, "فیلم دیدن")
+ todolist = append(todolist, "پختن غذا")
 
-	fmt.Println("سرور در حال اجرا روی پورت 8080...")
-	http.ListenAndServe(":8080", nil)
+
+ _, err = collection.InsertMany(context.TODO(), []interface{}{todolist})
+ if err != nil {
+  log.Fatal("نمی‌تونم لیست رو ذخیره کنم! خطا:", err)
+ }
+
+ fmt.Println("کارهایی که امروز باید انجام بدم:")
+ for i, harkar := range todolist {
+  fmt.Println(i+1,harkar)
+ }
+
+
+ http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+  if r.Method == http.MethodGet {
+   w.Header().Set("Content-Type", "application/json")
+   if err := json.NewEncoder(w).Encode(todolist); err != nil {
+    http.Error(w, "خطا تو تبدیل به JSON", http.StatusInternalServerError)
+    return
+   }
+  } else {
+   http.Error(w, "فقط می‌تونی GET کنی!", http.StatusMethodNotAllowed)
+  }
+ })
+
+ fmt.Println("وب‌سایت من روشنه! برو به پورت 8080...")
+ if err := http.ListenAndServe(":8080", nil); err != nil {
+  log.Fatal("وب‌سایت کار نمی‌کنه! خطا:", err)
+ }
 }
