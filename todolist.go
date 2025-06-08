4fd6ea9080f533d50731fb01e7f3d83f62799cee
silvanasln <silvanasalsabilian@gmail.com>
2025-06-08 16:22:40 +0330

    Update todolist.go

--- todolist.go
+++ todolist.go
@@ -1,142 +1,144 @@
 package main
 
 import (
- "context"
- "encoding/json"
- "log"
- "net/http"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
- "github.com/labstack/echo/v4"
- "go.mongodb.org/mongo-driver/bson"
- "go.mongodb.org/mongo-driver/mongo"
- "go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Task struct {
- ID   string json:"id" bson:"_id,omitempty"
- Name string json:"name" bson:"name"
+
+type Todo struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Title     string             `bson:"title" json:"title"`
+	Completed bool               `bson:"completed" json:"completed"`
 }
+
+var collection *mongo.Collection
 
 func main() {
- 
- client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
- if err != nil {
-  log.Fatal("وصل نیست, err)
- }
- defer client.Disconnect(context.TODO())
+ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal("خطا در اتصال به مونگو:", err)
+	}
+	defer client.Disconnect(context.Background())
 
- db := client.Database("todoDB")
- collection := db.Collection("tasks")
-
-
- todolist := []string{"خرید کردن", "کتاب خوندن", "فیلم دیدن", "پختن غذا"}
- for _, task := range todolist {
-  _, err := collection.InsertOne(context.TODO(), Task{Name: task})
-  if err != nil {
-   log.Fatal("نمی‌تونم ذخیره کنم خطا:", err)
-  }
- }
-
-
- tasks, err := getAllTasks(collection)
- if err != nil {
-  log.Fatal("نمی‌تونم وظایف رو بگیرم خطا:", err)
- }
- fmt.Println("کارهایی که امروز باید انجام بدم:")
- for i, task := range tasks {
-  fmt.Println(i+1, task.Name)
- }
+	collection = client.Database("todoDB").Collection("tasks")
 
  
+	count, _ := collection.CountDocuments(context.Background(), bson.M{})
+	if count == 0 {
+		todos := []interface{}{
+			Todo{Title: "خرید کردن", Completed: false},
+			Todo{Title: "کتاب خوندن", Completed: false},
+			Todo{Title: "فیلم دیدن", Completed: false},
+			Todo{Title: "پختن غذا", Completed: false},
+		}
+		_, err := collection.InsertMany(context.Background(), todos)
+		if err != nil {
+			log.Println("نمی‌تونم لیست اولیه رو ذخیره کنم! خطا:", err)
+		} else {
+			fmt.Println("لیست اولیه ثبت شد.")
+		}
+	}
+
+
  e := echo.New()
 
- 
- e.GET("/todos", getTodos(collection))
- e.POST("/todos", createTodo(collection))
- e.PUT("/todos/:id", updateTodo(collection))
- e.DELETE("/todos/:id", deleteTodo(collection))
+	e.GET("/todos", getTodos)
+	e.POST("/todos", createTodo)
+	e.PUT("/todos/:id", updateTodo)
+	e.DELETE("/todos/:id", deleteTodo)
 
-
- fmt.Println("وب‌سایت من روشنه! برو به پورت 8080...")
- if err := e.Start(":8080"); err != nil {
-  log.Fatal("وب‌سایت کار نمی‌کنه! خطا:", err)
- }
+	fmt.Println("وب‌سایت من روشنه! برو به پورت 8080...")
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 
-func getAllTasks(collection *mongo.Collection) ([]Task, error) {
- var tasks []Task
- cursor, err := collection.Find(context.TODO(), bson.M{})
- if err != nil {
-  return nil, err
- }
- defer cursor.Close(context.TODO())
+func getTodos(c echo.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "خطا در خواندن کارها")
+	}
+	defer cursor.Close(ctx)
 
- for cursor.Next(context.TODO()) {
-  var task Task
-  if err := cursor.Decode(&task); err != nil {
-   return nil, err
-  }
-  tasks = append(tasks, task)
- }
- return tasks, nil
+	var todos []Todo
+	for cursor.Next(ctx) {
+		var t Todo
+		if err := cursor.Decode(&t); err != nil {
+			return c.JSON(http.StatusInternalServerError, "خطا در دیکد کردن داده")
+		}
+		todos = append(todos, t)
+	}
+	return c.JSON(http.StatusOK, todos)
 }
 
 
-func getTodos(collection *mongo.Collection) echo.HandlerFunc {
- return func(c echo.Context) error {
-  tasks, err := getAllTasks(collection)
-  if err != nil {
-   return c.JSON(http.StatusInternalServerError, map[string]string{"error": "خطا در دریافت "})
-  }
-  return c.JSON(http.StatusOK, tasks)
- }
+
+func createTodo(c echo.Context) error {
+	var todo Todo
+	if err := c.Bind(&todo); err != nil {
+		return c.JSON(http.StatusBadRequest, "ورودی نامعتبر")
+	}
+	todo.Completed = false
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := collection.InsertOne(ctx, todo)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "خطا در ذخیره کار")
+	}
+	todo.ID = res.InsertedID.(primitive.ObjectID)
+	return c.JSON(http.StatusCreated, todo)
 }
 
 
-func createTodo(collection *mongo.Collection) echo.HandlerFunc {
- return func(c echo.Context) error {
-  var task Task
-  if err := c.Bind(&task); err != nil {
-   return c.JSON(http.StatusBadRequest, map[string]string{"error": "داده نامعتبره"})
-  }
-  if task.Name == "" {
-   return c.JSON(http.StatusBadRequest, map[string]string{"error": "نام نمی‌تونه خالی باشه"})
-  }
-
-  _, err := collection.InsertOne(context.TODO(), task)
-  if err != nil {
-   return c.JSON(http.StatusInternalServerError, map[string]string{"error": "نمی‌تونم ذخیره کنم"})
-  }
-  return c.JSON(http.StatusCreated, task)
- }
+func updateTodo(c echo.Context) error {
+	idParam := c.Param("id")
+	todoID, err := primitive.ObjectIDFromHex(idParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "شناسه نامعتبر")
+	}
+	var todo Todo
+	if err := c.Bind(&todo); err != nil {
+		return c.JSON(http.StatusBadRequest, "ورودی نامعتبر")
+	}
+	update := bson.M{
+		"$set": bson.M{
+			"title":     todo.Title,
+			"completed": todo.Completed,
+		},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err = collection.UpdateOne(ctx, bson.M{"_id": todoID}, update)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "خطا در ویرایش کار")
+	}
+	return c.JSON(http.StatusOK, "کار به‌روزرسانی شد")
 }
 
-
-func updateTodo(collection *mongo.Collection) echo.HandlerFunc {
- return func(c echo.Context) error {
-  id := c.Param("id")
-  var task Task
-  if err := c.Bind(&task); err != nil {
-   return c.JSON(http.StatusBadRequest, map[string]string{"error": " نامعتبره"})
-  }
-
-  update := bson.M{"$set": bson.M{"name": task.Name}}
-  _, err := collection.UpdateOne(context.TODO(), bson.M{"_id": id}, update)
-  if err != nil {
-   return c.JSON(http.StatusInternalServerError, map[string]string{"error": "نمی‌تونم به‌روزرسانی کنم"})
-  }
-  return c.JSON(http.StatusOK, task)
- }
-}
-
-func deleteTodo(collection *mongo.Collection) echo.HandlerFunc {
- return func(c echo.Context) error {
-  id := c.Param("id")
-  _, err := collection.DeleteOne(context.TODO(), bson.M{"_id": id})
-  if err != nil {
-   return c.JSON(http.StatusInternalServerError, map[string]string{"error": "نمی‌تونم حذف کنم"})
-  }
-  return c.NoContent(http.StatusNoContent)
- }
+func deleteTodo(c echo.Context) error {
+	idParam := c.Param("id")
+	todoID, err := primitive.ObjectIDFromHex(idParam)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "شناسه نامعتبر")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err = collection.DeleteOne(ctx, bson.M{"_id": todoID})
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "خطا در حذف کار")
+	}
+	return c.JSON(http.StatusOK, "کار حذف شد")
 }
